60ca05fac4c08e856d581bd543622e28fae9e16a
Veronika Kim <seeker8529@MacBook-Air-Veronika.local>
2021-01-22 22:40:45 +0300

    Lab 4 fixed: added complex search

--- lab1/user.js
+++ lab1/user.js
@@ -1,7 +1,9 @@
 
 $(document).ready(function() {
   $('#accept_choice').hide();
-  $('#save_history').hide();
+  $('#select_one').hide();
+  $('#select_several').hide();
+  $('#select_parametric').hide();
   $('#save').hide();
   $('#second').hide();
   
@@ -25,6 +27,10 @@ $(document).ready(function() {
 
   $("#get_tree_button").click(function(){
     window.location.href='tree.html';
+  });
+  
+  $("#metrics").click(function(){
+    window.location.href='metrics.html';
   });
   
   $("#back").click(function(){
@@ -66,22 +72,61 @@ $(document).ready(function() {
       $('#liked').html(txt);
 	  $('#disliked').html(txt1);
       $('.recomendation').hide();
-      $('#accept_choice').show();
-      $('#save_history').show();
       $('#save').show();
 
    });
 
   $('#accept_choice').click(function () {
+  
+    //Выбираем все отмеченные checkbox
+    var a=$('input[name="group1"]:checked'); 
+    var checked=[];
+    for (var x=0; x<a.length;x++){
+            checked.push(a[x].value);
+    }
+    console.log(checked);
+    
+    //Собираем массив массивов рекомендаций для каждого выделенного объекта
+    var list_of_lists=[];
+
+    for (let i=0; i<checked.length; i++){
+       let list=[];
+       let wrk_leave = find_by_name(checked[i], root);
+       recommendation_list(wrk_leave, root, list);
+       list.sort((a, b) => a.val > b.val ? 1 : -1);
+
+       list_of_lists.push(list.slice(1, 15));
+    }
+
+    var local_recom_list=get_common_recomendations(list_of_lists);
 
 
+    for (let i=0; i<checked.length; i++){
+        local_recom_list = del_el(local_recom_list, checked[i]);
+    }
+    
+    if (recom_list.length==0){
+    
+     for (let i=0; i<checked.length; i++){
+        recom_list = del_el(recom_list, checked[i]);
+     }
+
+     local_recom_list = get_by_history(local_recom_list, recom_list);
+      
+     for (let i=0; i<disliked.length; i++){
+        local_recom_list = del_el(local_recom_list, disliked[i]);
+      }
+    }
+    
+   local_recom_list = local_recom_list.slice(1,20);
+   
    $('#accept_choice').hide();
    $("#save_history").hide();
    $("#save").hide();
    
    var txt="Вам могут понравиться:";
 
-     recom_list.forEach(function(item, i, arr){
+     local_recom_list.forEach(function(item, i, arr){
      txt = txt+'<br/>'+item.name;
    });
    
@@ -136,12 +181,53 @@ $(document).ready(function() {
     recom_list = recom_list.slice(1,20);
     console.log(recom_list);
     disliked = checked_b;
+    
+    $('#liked').hide();
+    $('#disliked').hide();
+    $('#save').hide();
+    $('#select_one').show();
+  	$('#select_several').show();
+  	$('#select_parametric').show();
+
   });
    
-  $("#save_history").click(function(){ 
+  $("#select_one").click(function(){ 
   
    	$('#first').hide();
    	$('#second').show();
+   	$('#select_one').hide();
+  	$('#select_several').hide();
+  	$('#select_parametric').hide();
+   });
+   
+  $("#select_several").click(function(){ 
+  
+   $('#liked').show();
+  
+      let list = [];
+      get_all(root, list);
+
+      let txt="Выберете из списка:<br/>";
+      console.log(list);
+
+      list.forEach(function(item, i, arr){
+        console.log(item.name);
+        txt = txt + '<br/>' +
+            '<div class="form-check form-check-inline" >\n' +
+            '  <input class="form-check-input" type="checkbox" name="group1" value="' + item + '">' +
+            '<div>' +
+            '  <label class="form-check-label">' + item + '</label>'+
+            '</div>' +
+            '</div>';
+      });
+      
+    $('#liked').html(txt);
+	$('#accept_choice').show();
+	$('#disliked').hide;
+   	$('#second').hide();
+   	$('#select_one').hide();
+  	$('#select_several').hide();
+  	$('#select_parametric').hide();
    });
    
   $("#go_button").click(function(){
@@ -186,4 +272,6 @@ $(document).ready(function() {
   });  
    
    
+   
 });
+
